6d57f52c964a298853ce861487c89fd4d92d1e39
josh182014 <josh182014@gmail.com>
2019-04-02 15:16:57 -0600

    completed closure challenge 1

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,20 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function SW() {
+  const vador = "Darth Vador";
+  console.log(`${vador}: I am your father`);
+  debugger;
+  
+  function SWA() {
+    const anakin = "Anakin";
+    console.log(`${anakin} to ${vador}: No. No! That’s not true! That’s impossible!`);
+  
+  }
+  SWA();
+}
+SW();
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
